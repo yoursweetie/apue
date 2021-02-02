9243d7d60e2a572ae23c808a3218610d5fdd846a
yoursweetie <939696280@qq.com>
2021-02-02 20:19:06 +0800

    test for table

--- err_handling.c
+++ err_handling.c
@@ -6,17 +6,21 @@
 int
 main()
 {
-	int fd;
-	fd = open("./helloopen.txt", O_RDWR);
-	//printf("open fd = %d\n", fd);
-	printf("open errno = %d\n", errno);
-	perror("open");
-	char hello[] = "hello open!\n";
-	//printf("sizeof(hello array) = %d\n",sizeof(hello));	!    !
-	int a =	write(fd, hello, sizeof(hello) - 1);
-	perror("write");
-	close(fd);
-	printf("write bytes = %d\n", a);
-	printf("errno = %d\n", errno);
-	return 0;
+    int fd;
+
+
+    fd = open("./helloopen.txt", O_RDWR);
+
+    //printf("open fd = %d\n", fd);
+    printf("open errno = %d\n", errno);
+    perror("open");
+    char hello[] = "hello open!\n";
+    //printf("sizeof(hello array) = %d\n",sizeof(hello));
+
+    int a = write(fd, hello, sizeof(hello) - 1);
+    perror("write");
+    close(fd);
+    printf("write bytes = %d\n", a);
+    printf("errno = %d\n", errno);
+    return 0;
 }
