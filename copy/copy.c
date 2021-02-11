999ed84ff3f01f0e2eb0aabf86027da7ebdc6891
yoursweetie <939696280@qq.com>
2021-02-11 23:19:40 +0800

    fix format

--- copy/copy.c
+++ copy/copy.c
@@ -3,21 +3,21 @@
 int
 main()
 {
-	int n;
-	char buf[BUFFSIZE];
+    int n;
+    char buf[BUFFSIZE];
 
-	while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
-	{
-		if (write(STDOUT_FILENO, buf, n) < 0)
-		{
-			err_sys("write error!");
-		}
-	}
+    while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
+    {
+        if (write(STDOUT_FILENO, buf, n) < 0)
+        {
+            err_sys("write error!");
+        }
+    }
 
-	if (n < 0)
-	{
-		err_sys("read error!");
-	}
+    if (n < 0)
+    {
+        err_sys("read error!");
+    }
 
-	exit(0);
+    exit(0);
 }
