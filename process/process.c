40c54fce9b67967f567ae38036f98067bfd1282c
yoursweetie <939696280@qq.com>
2021-02-13 23:05:16 +0800

    fix format

--- process/process.c
+++ process/process.c
@@ -4,34 +4,32 @@
 int
 main()
 {
-	char buf[MAXLINE];
-	pid_t pid;
-	int status;
+    char buf[MAXLINE];
+    pid_t pid;
+    int status;
 
-	printf("%% ");
-	while (fgets(buf, MAXLINE, stdin) != NULL)
-	{
-		if (buf[strlen(buf) - 1] == '\n')
-		{
-			buf[strlen(buf) - 1] = 0;
-		}
-
-		if ((pid = fork()) < 0)
-		{
-			err_sys("fork error");
-		}
-	   	else if (pid == 0)
-		{
-			execlp(buf, buf, (char *)0);
-			err_ret("couldn't execute\n");
-			exit(127);
-		}
-
-		if ((pid = waitpid(pid, &status, 0)) < 0)
-		{
-			err_sys("waitpid error");
-		}
-		printf("%% ");
-	}
-	exit(0);
+    printf("%% ");
+    while (fgets(buf, MAXLINE, stdin) != NULL)
+    {
+        if (buf[strlen(buf) - 1] == '\n')
+        {
+            buf[strlen(buf) - 1] = 0;
+        }
+            if ((pid = fork()) < 0)
+        {
+            err_sys("fork error");
+        }
+        else if (pid == 0)
+        {
+            execlp(buf, buf, (char *)0);
+            err_ret("couldn't execute\n");
+            exit(127);
+        }
+            if ((pid = waitpid(pid, &status, 0)) < 0)
+        {
+            err_sys("waitpid error");
+        }
+        printf("%% ");
+    }
+    exit(0);
 }
